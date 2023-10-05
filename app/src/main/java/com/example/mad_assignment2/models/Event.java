25fe9be997b284e96906d5eb558e848f55c44438
yvonn <yvonnechua2002@gmail.com>
2023-10-05 14:48:58 +0800

    add calendar screen files

--- app/src/main/java/com/example/mad_assignment2/models/Event.java
+++ app/src/main/java/com/example/mad_assignment2/models/Event.java
@@ -9,6 +9,7 @@ public class Event {
     String date;
     String time;
 
+
     public Event(String t, String d, String o, String b, String l, String da, String tim) {
         this.title = t;
         this.description = d;
