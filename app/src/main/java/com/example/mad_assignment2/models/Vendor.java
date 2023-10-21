8c7fb1e51c3128edb43b3cea4d3b5fa8be76abf9
bonifacioronald <bonifacioronald99@gmail.com>
2023-10-21 22:40:24 +0800

    feat:fix event and vendor data model

--- app/src/main/java/com/example/mad_assignment2/models/Vendor.java
+++ app/src/main/java/com/example/mad_assignment2/models/Vendor.java
@@ -1,21 +1,97 @@
 package com.example.mad_assignment2.models;
 
 public class Vendor {
-    String id;
-    String name;
-    String email;
-    String password;
-    String description;
-    String category;
-    String logoUrl;
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String description;
+    private String category;
+    private String imageUrl;
+    private double rating;
+    private int boothLocation;
 
-    public Vendor(String i, String n, String e, String p, String d, String c, String l) {
-        this.id = i;
-        this.name = n;
-        this.email = e;
-        this.password = p;
-        this.description = d;
-        this.category = c;
-        this.logoUrl = l;
+    public Vendor(String id, String name, String email, String password, String description, String category, String imageUrl, double rating, int boothLocation) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.description = description;
+        this.category = category;
+        this.imageUrl = imageUrl;
+        this.rating = rating;
+        this.boothLocation = boothLocation;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getBoothLocation() {
+        return boothLocation;
+    }
+
+    public void setBoothLocation(int boothLocation) {
+        this.boothLocation = boothLocation;
     }
 }
