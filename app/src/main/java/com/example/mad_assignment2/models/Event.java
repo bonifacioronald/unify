0af5bac54782d502ccc2d2e93772ebd748ea8ff0
yvonn <yvonnechua2002@gmail.com>
2023-10-29 22:49:17 +0800

    update event.java

--- app/src/main/java/com/example/mad_assignment2/models/Event.java
+++ app/src/main/java/com/example/mad_assignment2/models/Event.java
@@ -1,21 +1,100 @@
 package com.example.mad_assignment2.models;
 
-public class Event {
-    String title;
-    String description;
-    String organizer;
-    String backgroundLogoUrl;
-    String layoutUrl;
-    String date;
-    String time;
+import java.util.ArrayList;
+import java.util.Date;
 
-    public Event(String t, String d, String o, String b, String l, String da, String tim) {
-        this.title = t;
-        this.description = d;
-        this.organizer = o;
-        this.backgroundLogoUrl = b;
-        this.layoutUrl = l;
-        this.date = d;
-        this.time = tim;
+public class Event {
+    private String id;
+    private String title;
+    private String description;
+    private String organizer;
+    private String backgroundLogoUrl;
+    private Date startDate;
+    private Date endDate;
+    private String time;
+    private ArrayList<String> vendorIdList;
+
+    public Event(String id, String title, String description, String organizer, String backgroundLogoUrl, Date startDate, Date endDate, String time, ArrayList<String> vendorIdList) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.organizer = organizer;
+        this.backgroundLogoUrl = backgroundLogoUrl;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.time = time;
+        this.vendorIdList = vendorIdList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    public String getBackgroundLogoUrl() {
+        return backgroundLogoUrl;
+    }
+
+    public void setBackgroundLogoUrl(String backgroundLogoUrl) {
+        this.backgroundLogoUrl = backgroundLogoUrl;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public ArrayList<String> getVendorIdList() {
+        return vendorIdList;
+    }
+
+    public void setVendorIdList(ArrayList<String> vendorIdList) {
+        this.vendorIdList = vendorIdList;
     }
 }
