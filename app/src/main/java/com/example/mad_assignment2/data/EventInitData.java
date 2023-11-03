55c0d7de2ad04d9ffe1766b167b3d518c1a851d0
bonifacioronald <bonifacioronald99@gmail.com>
2023-11-03 09:10:52 +0800

    fix: change event booking calendar months to nov and dec 2023

--- app/src/main/java/com/example/mad_assignment2/data/EventInitData.java
+++ app/src/main/java/com/example/mad_assignment2/data/EventInitData.java
@@ -20,8 +20,8 @@ public class EventInitData {
                 "Browse through and learn more about Taylor’s University’s diverse clubs and societies. Free of charge!",
                 "Orientation Leaders",
                 "https://selangor.travel/wp-content/uploads/2019/09/Taylors_College_Lakeside_Tourism_Selangor.jpg",
-                new Date(String.valueOf(dateFormat.parse("20-11-2023"))),
-                new Date(String.valueOf(dateFormat.parse("25-11-2023"))),
+                new Date(String.valueOf(dateFormat.parse("16-11-2023"))),
+                new Date(String.valueOf(dateFormat.parse("23-11-2023"))),
                 "8:00 AM - 5:00 PM",
                 new ArrayList<String>());
 
@@ -37,8 +37,8 @@ public class EventInitData {
                 "Taylor's University celebrates its diverse culture with a traditional food festival featuring a tantalizing array of authentic dishes from around the world",
                 "TU Student Council",
                 "https://travelfoodatlas.com/wp-content/uploads/2021/11/malaysian-food.jpg.webp",
-                new Date(String.valueOf(dateFormat.parse("27-11-2023"))),
-                new Date(String.valueOf(dateFormat.parse("30-11-2023"))),
+                new Date(String.valueOf(dateFormat.parse("4-12-2023"))),
+                new Date(String.valueOf(dateFormat.parse("8-12-2023"))),
                 "8:00 AM - 5:00 PM",
                 new ArrayList<>());
 
