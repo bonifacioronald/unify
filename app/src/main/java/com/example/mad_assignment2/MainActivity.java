13556ef12472b9299f15272a1c001710530ba664
yvonn <yvonnechua2002@gmail.com>
2023-11-09 20:49:27 +0800

    update layout

--- app/src/main/java/com/example/mad_assignment2/MainActivity.java
+++ app/src/main/java/com/example/mad_assignment2/MainActivity.java
@@ -9,6 +9,8 @@ import android.util.Log;
 import com.example.mad_assignment2.data.EventDBHelper;
 import com.example.mad_assignment2.data.VendorDBHelper;
 import com.example.mad_assignment2.models.Event;
+import com.example.mad_assignment2.screens.DirectoryActivity;
+import com.example.mad_assignment2.screens.DirectoryDetailActivity;
 import com.example.mad_assignment2.screens.VendorBookingCalendarScreen;
 import com.example.mad_assignment2.screens.VendorLoginScreen;
 
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
         super.onCreate(savedInstanceState);
-        Intent myIntent = new Intent(MainActivity.this, VendorLoginScreen.class);
+        Intent myIntent = new Intent(MainActivity.this, DirectoryActivity.class);
         MainActivity.this.startActivity(myIntent);
     }
 }
