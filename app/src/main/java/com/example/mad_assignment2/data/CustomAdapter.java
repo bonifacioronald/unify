7329f7edde13e45a3578451c77e6494eae774285
Alvin Tang <alvintang0520@gmail.com>
2023-11-15 00:09:36 +0800

    Glide Done

--- app/src/main/java/com/example/mad_assignment2/data/CustomAdapter.java
+++ app/src/main/java/com/example/mad_assignment2/data/CustomAdapter.java
@@ -1,42 +1,36 @@
 package com.example.mad_assignment2.data;
 
 
-
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.example.mad_assignment2.R;
-import com.example.mad_assignment2.screens.EventPreviewScreen;
 import com.example.mad_assignment2.screens.VendorDetailScreen;
-
 
 import java.util.ArrayList;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
 
     private Context context;
-    private ArrayList name, description, category, imageUrl, rating, boothLocation;
-
-    int position;
+    private ArrayList<String> name, description, category, imageUrl, rating, boothLocation;
 
     public CustomAdapter(Context context,
-                         ArrayList name,
-                         ArrayList description,
-                         ArrayList category,
-                         ArrayList imageUrl,
-                         ArrayList rating,
-                         ArrayList boothLocation
-                         ){
-
+                         ArrayList<String> name,
+                         ArrayList<String> description,
+                         ArrayList<String> category,
+                         ArrayList<String> imageUrl,
+                         ArrayList<String> rating,
+                         ArrayList<String> boothLocation) {
         this.context = context;
         this.name = name;
         this.description = description;
@@ -45,30 +39,30 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         this.rating = rating;
         this.boothLocation = boothLocation;
     }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.my_row,parent,false);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CustomAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+    public void onBindViewHolder(@NonNull CustomAdapter.MyViewHolder holder, int position) {
+        holder.vendor_name_txt.setText(name.get(position));
+        holder.imageUrl_txt.setText(category.get(position));
 
-        holder.vendor_name_txt.setText(String.valueOf(name.get(position)));
-        holder.imageUrl_txt.setText(String.valueOf(category.get(position)));
-        holder.eventDetailsLayout.setOnClickListener(new View.OnClickListener(){
+        // Use Glide to load the image
+        Glide.with(context)
+                .load(imageUrl.get(position)) // Assuming imageUrl is a valid resource identifier
+                .into(holder.imageView2);
 
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(context, VendorDetailScreen.class);
-                intent.putExtra("CLICKED_VENDOR_NAME", String.valueOf(name.get(position)));
-                context.startActivity(intent);
-
-            }
+        holder.eventDetailsLayout.setOnClickListener(view -> {
+            Intent intent = new Intent(context, VendorDetailScreen.class);
+            intent.putExtra("CLICKED_VENDOR_NAME", name.get(position));
+            context.startActivity(intent);
         });
-
     }
 
     @Override
@@ -80,13 +74,14 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
         TextView vendor_name_txt, imageUrl_txt;
         LinearLayout eventDetailsLayout;
+        ImageView imageView2;
 
         public MyViewHolder(@NonNull View itemView) {
-
             super(itemView);
             vendor_name_txt = itemView.findViewById(R.id.vendor_name_txt);
             imageUrl_txt = itemView.findViewById(R.id.imageUrl_txt);
             eventDetailsLayout = itemView.findViewById(R.id.eventDetailsLayout);
+            imageView2 = itemView.findViewById(R.id.imageView2);
         }
     }
 }
--- app/src/main/java/com/example/mad_assignment2/screens/EventDetailScreen.java
+++ app/src/main/java/com/example/mad_assignment2/screens/EventDetailScreen.java
@@ -2,26 +2,24 @@ package com.example.mad_assignment2.screens;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.mad_assignment2.MainActivity;
+import com.bumptech.glide.Glide;
 import com.example.mad_assignment2.R;
 import com.example.mad_assignment2.data.CustomAdapter;
 import com.example.mad_assignment2.data.EventDBHelper;
 import com.example.mad_assignment2.data.VendorDBHelper;
 import com.example.mad_assignment2.models.Event;
 import com.example.mad_assignment2.models.Vendor;
-
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -30,6 +28,8 @@ public class EventDetailScreen extends AppCompatActivity {
 
     RecyclerView recyclerView;
     Button backButton;
+
+    ImageView vendorImageView;
 
     ImageButton previewImageButton;
 
@@ -57,11 +57,13 @@ public class EventDetailScreen extends AppCompatActivity {
         rating = new ArrayList<>();
         boothLocation = new ArrayList<>();
 
+
         try {
             storeVendorDataInArrays();
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+
 
         customAdapter = new CustomAdapter(EventDetailScreen.this, name, description, category, imageUrl, rating, boothLocation);
         recyclerView.setAdapter(customAdapter);
@@ -90,7 +92,7 @@ public class EventDetailScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(EventDetailScreen.this, EventPreviewScreen.class);
-                intent.putExtra("event_id",event_id);
+                intent.putExtra("event_id", event_id);
                 startActivity(intent);
             }
         });
@@ -110,15 +112,46 @@ public class EventDetailScreen extends AppCompatActivity {
             name.add(vendor.getName());
             description.add(vendor.getDescription());
             category.add(vendor.getCategory());
-            imageUrl.add(vendor.getImageUrl());
+            imageUrl.add(String.valueOf(vendor.getImageUrl()));
             rating.add(String.valueOf(vendor.getRating()));
             boothLocation.add(String.valueOf(vendor.getBoothLocation()));
         }
 
     }
 
+    private void setDetailContent(String vendorName) throws ParseException {
 
-}
+
+        ImageView imageView2 = findViewById(R.id.imageView2);
+
+
+
+            Vendor vendor = vendorDBHelper.getVendorByName(vendorName);
+
+
+            if (vendor != null) {
+
+                imageUrl.add(String.valueOf(vendor.getImageUrl()));
+
+                Log.d("EventDetailScreen", "Image URL Resource Name: " + imageUrl);
+
+
+                int imageUrlResourceId = getResources().getIdentifier(String.valueOf(imageUrl), "drawable", getPackageName());
+
+                Log.d("EventDetailScreen", "Image URL Resource ID: " + imageUrlResourceId);
+
+
+                Glide.with(this)
+                        .load(vendor.getImageUrl()) // Provide the URL for the image
+                        .placeholder(R.drawable.loading_indicator) // Optional: Placeholder image while loading
+                        .error(R.drawable.loading_indicator) // Optional: Image to display in case of error
+                        .into(imageView2);
+
+
+            }
+        }
+    }
+
 
 
 
